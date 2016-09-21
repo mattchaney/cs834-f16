691b6c294b32235b60665d0dbfb915559a513bda
Matt Chaney <mchaney@alionscience.com>
2016-09-20 20:40:54 -0400

    Adding 1.2 and 3.9.

---
+++ assignments/assignment1/code/crawler/crawler.py
@@ -0,0 +1,38 @@
+import argparse
+import requests
+from time import sleep
+from collections import deque
+from bs4 import BeautifulSoup
+
+
+def crawl():
+    while True:
+        url = frontier.popleft()
+        if url not in visited:
+            visited.add(url)
+            print 'retrieving', url
+            try:
+                res = requests.get(url)
+            except:
+                continue
+            if res.ok:
+                soup = BeautifulSoup(res.text, 'html.parser')
+                links = soup.find_all('a')
+                for link in links:
+                    try:
+                        linkurl = link['href'].strip()
+                        if linkurl not in visited:
+                            frontier.append(linkurl)
+                    except KeyError:
+                        continue
+        sleep(5)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('web crawler')
+    parser.add_argument('--host', default='http://cs.odu.edu/~mln')
+    args = parser.parse_args()
+    visited = set()
+    frontier = deque()
+    frontier.append(args.host)
+    crawl()
