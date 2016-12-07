162fd821eba3dfdec1e2b222995dfdec72f8a944
Matt Chaney <mchaney@alionscience.com>
2016-12-06 21:20:01 -0500

    adding q89, almost done

--- assignments/assignment4/code/getrel/getrel.py
+++ assignments/assignment4/code/getrel/getrel.py
@@ -8,6 +8,7 @@ from math import log
 from bs4 import BeautifulSoup
 
 def parseargs():
+    """parse command line arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--port', type=int, default=42247, help='galago server port')
     parser.add_argument('-q', '--qnum', nargs='+', type=int, default=[6, 8], help='query number')
@@ -17,6 +18,7 @@ def parseargs():
 args = parseargs()
 
 def buildrel():
+    """read CACM relevance judgments for each query"""
     rel = {}
     for line in open('cacm.rel').readlines():
         q, _, doc, _ = line.split()
@@ -26,6 +28,7 @@ def buildrel():
     return rel
 
 def buildqueries():
+    """read CACM query file into a map structure for later use"""
     with open('cacm.query.xml') as fd:
         return xmltodict.parse(fd.read())
 
@@ -38,6 +41,7 @@ QUERY1 = 'what articles exist which deal with tss time sharing system an operati
 PDICT = {'q': QUERY1, 'start': 0, 'n': args.n}
 
 def query(qstr, port=args.port):
+    """issue a query to a running Galago server"""
     PDICT['q'] = qstr
     PDICT['n'] = args.n
     res = requests.get(URL.format(port), params=PDICT)
@@ -47,22 +51,26 @@ def query(qstr, port=args.port):
     return [int(RE.match(href.text).groups()[0]) for href in soup.select("#result a")]
 
 def recall(rel, retr):
+    """calculate the recall given relevant and retrieved documents"""
     relset = set(rel)
     retrset = set(retr)
     return float(len(relset.intersection(retrset))) / len(relset)
 
 def precision(rel, retr):
+    """calculate the precision given relevant and retrieved documents"""
     relset = set(rel)
     retrset = set(retr)
     return float(len(relset.intersection(retrset))) / len(retrset)
 
 def run(rel, retr, func):
+    """calculate recall or precision at all ranks given relevant and retrieved documents"""
     rr = []
     for i in range(1, len(retr)+1):
         rr.append(func(rel, retr[:i]))
     return rr
 
 def avg(rel, retr, func):
+    """calculate avg recall or precision given relevant and retrieved documents"""
     prun = run(rel, retr, func)
     res = []
     for i in range(len(retr)):
@@ -73,32 +81,38 @@ def avg(rel, retr, func):
     return float(sum(res))/len(res)
 
 def getrel(rel, retr, i):
+    """get a relevancy score for a given retrieved document"""
     return 1 if retr[i] in rel else 0
 
 def DCG(rel, retr, p):
+    """calculate DCG at p given relevant and retrieved documents"""
     sum = 0
     for i in range(2, p+1):
         sum += float(getrel(rel, retr, i-1)) / log(i, 2)
     return getrel(rel, retr, 0) + sum
 
 def IDCG(p):
+    """calculate IDCG at p given relevant and retrieved documents"""
     sum = 0
     for i in range(2, p+1):
         sum += 1 / log(i, 2)
     return 1 + sum
 
 def NDCG(rel, retr, p):
+    """calculate NDCG at p given relevant and retrieved documents"""
     dcg = DCG(rel, retr, p)
     idcg = IDCG(p)
     return dcg / idcg
 
 def reciprank(rel, retr):
+    """calculate reciprocal rank given relevant and retrieved documents"""
     for i in range(1, len(retr)+1):
         if retr[i-1] in rel:
             return 1.0 / i
     return 0.0
 
 def ipr(rrun, prun):
+    """calculate interpolated precision at std recall given recall and precision runs"""
     res = []
     for i in np.arange(0, 1.1, .1):
         for j in range(len(rrun)):
@@ -109,12 +123,42 @@ def ipr(rrun, prun):
     return np.arange(0, 1.1, 0.1), res
 
 def rprecision(rel, prun):
+    """calculate R-precision"""
     return prun[len(rel)]
 
+def bpref1(rel, retr):
+    """calculate BPREF as the first equation in the textbook"""
+    relset = set(rel)
+    retrset = set(retr)
+    R = float(len(rel))
+    res = 0
+    for dr in rel:
+        if dr not in retr:
+            Ndr =  R
+        else:
+            Ndr = float(len([doc for doc in retr[:retr.index(dr)] if doc not in rel]))
+        res += (1.0 - Ndr/R)
+    return 1.0/R * res 
+
+def bpref2(rel, retr):
+    """calculate BPREF as the second equation in the textbook"""
+    p = 0.0
+    q = 0.0
+    for dr in rel:
+        if dr not in retr:
+            p += 0
+            q += len(retr)
+        else:
+            p += float(len([doc for doc in retr[retr.index(dr):] if doc not in rel]))
+            q += float(len([doc for doc in retr[:retr.index(dr)] if doc not in rel]))
+    return p / (p + q)
+
 def getquery(qnum):
+    """get the query string"""
     return QUERIES['parameters']['query'][qnum-1]['text']
 
 def process(qnum):
+    """run a number of calculations used for some exercises"""
     qstr = getquery(qnum)
     retr = query(qstr)
     if str(qnum) not in REL:
@@ -152,6 +196,7 @@ def printresults(qnum, qstr, retr, rel, prun, rrun, prec, rec, ndcg5, ndcg10, av
     print 'reciprocal rank: {0}'.format(recip)
 
 def printdata(rrun, prun, fname):
+    """print a data table for Rscript consumption"""
     with open(fname, 'w') as fd:
         zipped = zip(rrun, prun)
         for z in zipped:
--- assignments/assignment4/code/getrel/q89.py
+++ assignments/assignment4/code/getrel/q89.py
@@ -1,2 +1,51 @@
 from getrel import *
 
+HEAD = """\\begin{table}[H]
+\\centering
+\\begin{tabular}{ | l | l | l | }
+\\hline
+Query & BPREF(1) & BPREF(2) \\\\
+\\hline
+"""
+
+ROW = """{} & {} & {}\\\\
+\\hline
+"""
+
+TAIL = """\\end{tabular}
+\\caption{Table of BPREF values for a selection of CACM queries.}
+\\label{tab:bpref}
+\\end{table}
+"""
+
+def printtab(values):
+    with open('q89.tab', 'w') as fd:
+        fd.write(HEAD)
+        for qnum, b1, b2 in values:
+            fd.write(ROW.format(qnum, b1, b2))
+        fd.write(TAIL)
+
+def getsub(rel, retr):
+    """bound the size of retrieved list by R non-relevant documents"""
+    sub = []
+    count = 0
+    for doc in retr:
+        if doc not in rel:
+            count += 1
+        if count > len(rel):
+            break
+        sub.append(doc)
+    return sub
+
+values = []
+for qnum in args.qnum:
+    results = process(qnum)
+    retr, rel = results[2], results[3]
+    sub = getsub(rel, retr)
+    b1 = bpref1(rel, sub)
+    b2 = bpref2(rel, sub)
+    print 'Query:  {}'.format(qnum)
+    print 'BPREF1: {}'.format(b1)
+    print 'BPREF2: {}'.format(b2)
+    values.append((qnum, b1, b2))
+printtab(values)
